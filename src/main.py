407a74a5e628ba42ede9c33f940008ee4d69eec5
achourali <achourali1999@gmail.com>
2022-05-31 11:12:29 +0100

    not immuting original data

--- src/main.py
+++ src/main.py
@@ -9,31 +9,27 @@ def getMatches(leagueId):
 
 
 def cleanMatches(matches):
+    cleanData = []
     for match in matches:
-        if(match['finished']==False):
+        cleanMatch = {}
+        if(match['finished'] == False):
             matches.remove(match)
             continue
-        del(match['_id'])
-        del(match['date'])
-        del(match['flashscoreId'])
-        del(match['round'])
-        del(match['season'])
-        del(match['createdAt'])
-        del(match['updatedAt'])
-        del(match['__v'])
-        match['homeTeam'] = match['homeTeam']['name']
-        match['awayTeam'] = match['awayTeam']['name']
-        match['homeTeamGoals'] =len(list(filter(lambda goal: goal['homeTeam'] == True, match['goals'])))
-        match['awayTeamGoals'] =len(list(filter(lambda goal: goal['homeTeam'] == False, match['goals'])))
-        del(match['goals'])
+        cleanMatch['homeTeam'] = match['homeTeam']['name']
+        cleanMatch['awayTeam'] = match['awayTeam']['name']
+        cleanMatch['homeTeamGoals'] = len(
+            list(filter(lambda goal: goal['homeTeam'] == True, match['goals'])))
+        cleanMatch['awayTeamGoals'] = len(
+            list(filter(lambda goal: goal['homeTeam'] == False, match['goals'])))
+        cleanData.append(cleanMatch)
 
-    return
-
+    return cleanData
 
 
 
 
 if __name__ == "__main__":
     matches = getMatches('6250d82b81afe4381753aefa')
-    cleanMatches(matches) 
-    print(len(matches))
+    matches = cleanMatches(matches)
+    # target=createTarget(matches)
+    print(matches[0])
