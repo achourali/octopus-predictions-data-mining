c82a6452f1134e8bd949b542cb7c37a2d0403810
achourali <achourali1999@gmail.com>
2022-06-01 21:24:42 +0100

    cors allow *

--- src/server.py
+++ src/server.py
@@ -2,10 +2,10 @@ from predict_match import predict_match
 from flask import Flask
 from flask import jsonify
 from flask import request
-
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
-
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 @app.route('/',methods=['POST'])
 def hello():
