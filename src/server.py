f672ee60197f7d2ae1b257fc06832c0e2c9ddd36
achourali <achourali1999@gmail.com>
2022-06-01 19:27:27 +0100

    get params from post body and return json prediction

--- src/server.py
+++ src/server.py
@@ -1,18 +1,20 @@
 from predict_match import predict_match
 from flask import Flask
-import json
+from flask import jsonify
+from flask import request
+
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/',methods=['POST'])
 def hello():
-    leagueId = "6250d82b81afe4381753aefa"
-    homeTeam = 'tunisia'
-    awayTeam = 'france'
-    date = '2022-11-30T15:00:00.000Z'
-
-    print('test***************************')
-    print(predict_match(leagueId, homeTeam, awayTeam, date))
-    return json.dumps(predict_match(leagueId, homeTeam, awayTeam, date))
+    return jsonify(
+        predict_match(
+            request.get_json()['leagueId'],
+            request.get_json()['homeTeam'],
+            request.get_json()['awayTeam'],
+            request.get_json()['date']
+            )
+        )
 
